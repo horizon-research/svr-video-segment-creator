e1e200392f4599bda5d59209ee7c90177292dce9
chichunchen <chichunchen844@gmail.com>
2018-08-04 17:48:09 -0400

    Fix bug for summation

--- storage_calculator.py
+++ storage_calculator.py
@@ -1,7 +1,7 @@
 import os
 
 if __name__ == '__main__':
-    dest_lst = ['rhino-fov', 'elephant-fov', 'paris-fov', 'roller-fov', 'nyc-fov']
+    dest_lst = ['rhino-fov', 'elephant-fov', 'paris-fov', 'nyc-fov', 'roller-fov']
     percentages = [0.25, 0.50, 0.75, 1.0]
     size_in_bytes = 0
 
@@ -12,13 +12,15 @@ if __name__ == '__main__':
                 subdir = os.path.join(dest, subdir)
                 total_file_num = len(os.listdir(subdir))
                 file_num_we_need = int(round(total_file_num * percentage))
-                # print(file_num_we_need)
+                # print(file_num_we_need, total_file_num)
                 for filename in os.listdir(subdir):
                     if file_num_we_need > 0:
                         filename = os.path.join(subdir, filename)
                         file_num_we_need = file_num_we_need - 1
                         st = os.stat(filename)
+                        # print(st.st_size)
                         size_in_bytes = size_in_bytes + st.st_size
                     else:
                         break
             print(percentage, size_in_bytes)
+            size_in_bytes = 0
