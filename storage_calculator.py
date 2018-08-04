406f697be4fc0abddbb569bc6eacbfe425720cd9
chichunchen <chichunchen844@gmail.com>
2018-08-04 17:33:42 -0400

    Add script for calculating storage overhead with different amount of objects

---
+++ storage_calculator.py
@@ -0,0 +1,24 @@
+import os
+
+if __name__ == '__main__':
+    dest_lst = ['rhino-fov', 'elephant-fov', 'paris-fov', 'roller-fov', 'nyc-fov']
+    percentages = [0.25, 0.50, 0.75, 1.0]
+    size_in_bytes = 0
+
+    for dest in dest_lst:
+        print(dest)
+        for percentage in percentages:
+            for subdir in os.listdir(dest):
+                subdir = os.path.join(dest, subdir)
+                total_file_num = len(os.listdir(subdir))
+                file_num_we_need = int(round(total_file_num * percentage))
+                # print(file_num_we_need)
+                for filename in os.listdir(subdir):
+                    if file_num_we_need > 0:
+                        filename = os.path.join(subdir, filename)
+                        file_num_we_need = file_num_we_need - 1
+                        st = os.stat(filename)
+                        size_in_bytes = size_in_bytes + st.st_size
+                    else:
+                        break
+            print(percentage, size_in_bytes)
